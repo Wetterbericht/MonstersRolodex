c2042487a822ca0643c403f7d16426c1e1bf2fa7
Frederick <frederick.baier.berlin@outlook.de>
2020-01-19 21:13:00 +0100

    Updates

---
+++ precache-manifest.4324f083dd05d299d8c1b91275f846a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc396c61b7ff990b660e8011c5a45165",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "16d7767d2907af8d7fc9",
+    "url": "/MonstersRolodex/static/css/main.29dc47c0.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/MonstersRolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonstersRolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16d7767d2907af8d7fc9",
+    "url": "/MonstersRolodex/static/js/main.e3d50d5d.chunk.js"
+  },
+  {
+    "revision": "e5fbf05eb7b3cf2cb151",
+    "url": "/MonstersRolodex/static/js/runtime-main.2bd3aecc.js"
+  }
+]);
